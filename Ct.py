78e316d8638b3301147870a0d9000ea5daf3f4ac
Yassine-Serrar <79869219+Yassine-Serrar@users.noreply.github.com>
2021-03-03 11:00:53 -0800

    Add files via upload

---
+++ Ct.py
@@ -0,0 +1,20 @@
+#A list
+FamilyNames = ("Yassine", "Hassan", "Khadija", "Sharah")
+print(FamilyNames)
+#A tuple(I think)
+yumtuple = ("Cake", "Apple Pie", "Baklava", "Chicken", "Cake", "Couscous")
+print(yumtuple)
+#Favorite Places Dictionary
+favoriteplacesdict = {
+    "Place": "Rabat, Morocco",
+    "Last Visited": "Summer 2019",
+}
+print(favoriteplacesdict)
+#An if and else statement (I think)
+ApplePie = 50
+Baklava = 80
+
+if Baklava > ApplePie:
+    print("Baklava is better and tastier than Apple Pie... Change my mind.")
+else:
+    print("Jk lol You can't change my mind!")
